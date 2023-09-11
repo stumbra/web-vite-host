9c18182cd5b03289c79cdd573e363227d78d1489
Martynas Stumbra <50275578+stumbra@users.noreply.github.com>
2023-09-11 23:24:33 +0300

    updated the vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,28 @@ import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: "app",
-      remotes: {
-        remoteApp: "http://localhost:5001/assets/remoteEntry.js",
-      },
-      shared: ["react", "react-dom", "react-router-dom"],
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
-});
+export default ({ mode }) => {
+  console.log(mode);
+
+  return defineConfig({
+    plugins: [
+      react(),
+      federation({
+        name: "app",
+        remotes: {
+          remoteApp:
+            mode === "development"
+              ? "http://localhost:5001/assets/remoteEntry.js"
+              : "https://web-vite-remote.vercel.app/assets/remoteEntry.js",
+        },
+        shared: ["react", "react-dom", "react-router-dom"],
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+  });
+};
