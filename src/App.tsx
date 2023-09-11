1fd390849a39fbdccbb0100ea85d9cbe37e89dc5
Martynas Stumbra <50275578+stumbra@users.noreply.github.com>
2023-09-11 23:56:04 +0300

    bring back mfe lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Wrapper from "./pages/Wrapper";
 
-import Remote from "remoteApp/Remote";
+const Remote = React.lazy(() => import("remoteApp/Remote"));
 
 const router = createBrowserRouter([
   { path: "/", element: <Wrapper /> },
   {
     path: "/remote/*",
-    element: <Remote />,
+    element: (
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Remote />
+      </React.Suspense>
+    ),
   },
 ]);
 
