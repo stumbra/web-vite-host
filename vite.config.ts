06e2524e2d83bf38f5b7c80fa3fd24cf19b06b6e
Martynas Stumbra <50275578+stumbra@users.noreply.github.com>
2023-09-11 23:39:22 +0300

    remove unwanted console.log

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  console.log(mode);
-
   return defineConfig({
     plugins: [
       react(),
